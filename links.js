365efeaa9b16b738688b1806180081a629b008f5
Eliah Kagan <degeneracypressure@gmail.com>
2021-02-24 21:12:03 -0500

    open links in a new tab when in a frame
    This makes links work reasonably from the editor.p5js.org
    deployment.

---
+++ links.js
@@ -0,0 +1,20 @@
+// This file is part of coordinates, which visualizes a plane in Cartesian and
+// circular-polar coordinates.
+//
+// Written in 2020, 2021 by Eliah Kagan <degeneracypressure@gmail.com>.
+//
+// To the extent possible under law, the author(s) have dedicated all copyright
+// and related and neighboring rights to this software to the public domain
+// worldwide. This software is distributed without any warranty.
+//
+// You should have received a copy of the CC0 Public Domain Dedication along
+// with this software. If not, see
+// <http://creativecommons.org/publicdomain/zero/1.0/>.
+
+(function () {
+    if (window !== parent) return;
+
+    // We seem not to be hosted in the p5js editor, so open links in this tab.
+    document.getElementById('project-link').setAttribute('target', '_self');
+    document.getElementById('p5js-link').setAttribute('target', '_self');
+})();
